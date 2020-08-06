bba974d2a765772589204bcf6d4b05bd0520a696
secure-revert-8990 <yuliyambg@gmail.com>
2020-08-06 23:41:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,18 @@
 class Dog
+  def name= dogs_name
+    @name = dogs_name
+  end
   
+  def name
+    @name
+  end
+  
+  def bread= dogs_bread
+    @bread = dogs_bread
+  end
+  
+  def bread
+    @bread
+  end
+  
+end
